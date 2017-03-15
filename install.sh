adc56a1723c03b3c08011aed8b0cad38103c888a
guri3 <aloesquash.lock@gmail.com>
2017-03-15 15:58:43 +0900

    Add .gitconfig

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.zshenv ~/.zshenv
 ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+

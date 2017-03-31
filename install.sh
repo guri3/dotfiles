9bd5fab5bb2ae1ed91bb046e0b2afc56d51553b7
guri3 <aloesquash.lock@gmail.com>
2017-03-31 15:33:01 +0900

    Initialize tmux settings

--- install.sh
+++ install.sh
@@ -3,4 +3,5 @@ ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.zshenv ~/.zshenv
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
-
+ln -s ~/dotfiles/.bashrc ~/.bashrc
+ln -s ~/dotfiles/.bash_profile ~/.bash_profile

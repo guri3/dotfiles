25bb67f9793373fcd2ab1396acb849db60d096db
guri3 <aloesquash.lock@gmail.com>
2018-03-28 12:41:24 +0900

    Update gitconfig

--- install.sh
+++ install.sh
@@ -2,7 +2,8 @@ ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.zshenv ~/.zshenv
 ln -s ~/dotfiles/.zshrc ~/.zshrc
-ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/git/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/.bashrc ~/.bashrc
 ln -s ~/dotfiles/.bash_profile ~/.bash_profile
 ln -s ~/dotfiles/.pryrc ~/.pryrc
+ 
